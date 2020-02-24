a7382e98948725aa9500933bc49f6c563cdf1994
Shankarsan Ganai <shankershawn@gmail.com>
2020-02-25 01:41:17 +0530

    Added endpoint for download over SSL

--- src/main/java/shankarsan/reactive/controller/ReactiveController.java
+++ src/main/java/shankarsan/reactive/controller/ReactiveController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import shankarsan.reactive.service.ReactiveService;
@@ -36,6 +37,11 @@ public class ReactiveController {
 	@GetMapping(value = "calldb", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> calldb(@PathVariable Map<String, String> requestParamMap) {
 		return reactiveService.getDBData();
+	}
+	
+	@GetMapping(value = "download")
+	public void download(@RequestParam(name = "url") String url, @RequestParam(name = "nc_cat") String ncCat, @RequestParam(name = "nc_ohc") String ncOhc, @RequestParam(name = "oh") String oh, @RequestParam(name = "oe") String oe) {
+		reactiveService.download(url, ncCat, ncOhc, oh, oe);
 	}
 
 }
--- src/main/java/shankarsan/reactive/service/ReactiveServiceImpl.java
+++ src/main/java/shankarsan/reactive/service/ReactiveServiceImpl.java
@@ -3,15 +3,30 @@
  */
 package shankarsan.reactive.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.mongodb.client.AggregateIterable;
@@ -31,6 +46,7 @@ import io.reactivex.schedulers.Schedulers;
 public class ReactiveServiceImpl implements ReactiveService {
 	
 	@Autowired @Qualifier("mongodbCollection") private MongoCollection<Document> mongoConnection;
+	@Autowired private Environment env;
 
 	@Override
 	public void printService() {
@@ -74,5 +90,72 @@ public class ReactiveServiceImpl implements ReactiveService {
 			documentList.add(documentIterator.next().toJson());
 		}
 		return documentList;
+	}
+
+	@Override
+	public void download(String fileUrl, String ncCat, String ncOhc, String oh, String oe) {
+		//String fileUrl = "https://instagram.fhyd2-1.fna.fbcdn.net/v/t51.2885-15/e35/67279283_2776497445737040_5300287398704471267_n.jpg?_nc_ht=instagram.fhyd2-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=K7IBshnpZZ4AX9deqta&oh=3b08b314b5be3f6dda4be8e2c081f7c0&oe=5E859849";
+		Random random = new Random();
+		String filePath = env.getProperty("file.path") + random.nextInt() + env.getProperty("file.extn");
+		fileUrl = new StringBuilder(fileUrl).append("&").append(ncCat).append("&").append(ncOhc).append("&").append(oh).append("&").append(oe).toString();
+		TrustManager trustManager[] = new TrustManager[] {
+				new X509TrustManager() {
+					
+					@Override
+					public X509Certificate[] getAcceptedIssuers() {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					
+					@Override
+					public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+						// TODO Auto-generated method stub
+						
+					}
+				}
+		};
+		InputStream in = null;
+		FileOutputStream fileOS = null;
+		
+		try {
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustManager, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			
+			URL url = new URL(fileUrl);
+			URLConnection urlConn = url.openConnection();
+			
+			in = urlConn.getInputStream();
+			fileOS = new FileOutputStream(new File(filePath));
+			
+			byte[] buffer = new byte[2048];
+			int length = 0;
+			while((length = in.read(buffer)) != -1) {
+				fileOS.write(buffer, 0, length);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(null != fileOS) {
+				try {
+					fileOS.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(null != in) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
